7bbcf271d7af4deccc6f8b720190a8bb41816d98
colinh0052 <144948776+colinh0052@users.noreply.github.com>
2026-02-11 19:03:42 -0600

    More Code

--- file.java
+++ file.java
@@ -1,6 +1,17 @@
-public class File{
+import java.util.Scanner;
+
+public class file{
     public static void main(String[] args) {
         // Your code goes here
-        Scanner myObj = new Scanner(System.in);
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String username = keyboard.nextLine();
+
+        System.out.println("Hello " + username);
+
+        System.out.println("I'm not very good at Java!");
+
+        keyboard.close();
     }
 }
